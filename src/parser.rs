d34ce1d3c6088098e52e7f1580e5ec9a6a8e132f
r_muramatsu <r_muramatsu@codmon.co.jp>
2025-01-13 21:20:31 +0900

    fix mod

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,6 @@
-mod comment_paser;
-mod converter;
-mod comment_node_collect;
+pub mod comment_paser;
+pub mod comment_node_collect;
+pub mod converter;
 
 pub fn get_comment(code: &str) -> Vec<String> {
     comment_paser::get_comments(code)
